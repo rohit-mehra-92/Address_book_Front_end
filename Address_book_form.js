2484f47f022d92fdf7c6b05bb89cbbcbb34b5ea8
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-06-23 05:01:22 +0530

    Added:save function and storing to local storage

--- Address_book_form.js
+++ Address_book_form.js
@@ -1,64 +1,130 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   
     const name = document.querySelector("#name");
+    const nameError = document.querySelector('.name-error')
     name.addEventListener("input", function () {
       if (name.value.length == 0) {
-        setTextValue(".name-error", "");
+        nameError.textContent = "";
         return;
       }
       try {
         new Contact().name = name.value;
-        setTextValue(".name-error", "");
+        nameError.textContent = "";
       } catch (error) {
-        setTextValue(".name-error", error);
+        nameError.textContent = error;
       }
     });
   
     const phoneNumber = document.querySelector("#phoneNumber");
+    const telError = document.querySelector('.tel-error')
     phoneNumber.addEventListener("input", function () {
       if (phoneNumber.value.length == 0) {
-        setTextValue(".tel-error", "");
-        return;
+       telError.textContent = "";
+       return;
       }
       try {
         new Contact().phoneNumber = phoneNumber.value;
-        setTextValue(".tel-error", "");
+        telError.textContent = "";
       } catch (error) {
-        setTextValue(".tel-error", error);
-      }
+        telError.textContent = error;
+    }
     });
   
     const address = document.querySelector("#address");
+    const addError = document.querySelector('address-error')
     address.addEventListener("input", function () {
       if (address.value.length == 0) {
-        setTextValue(".address-error", "");
+        addError.textContent = " ";
         return;
       }
       try {
         new Contact().address = address.value;
-        setTextValue(".address-error", "");
+        addError.textContent = " ";
       } catch (error) {
-        setTextValue(".address-error", error);
+        addError.textContent = error;
       }
     });
   
     const zip = document.querySelector("#zip");
+    const zipError = document.querySelector('.zip-error')
     zip.addEventListener("input", function () {
       if (zip.value.length == 0) {
-        setTextValue(".zip-error", "");
+        zipError.textContent = "";
         return;
       }
       try {
         new Contact().zip = zip.value;
-        setTextValue(".zip-error", "");
-      } catch (error) {
-        setTextValue(".zip-error", error);
-      }
+        zipError.textContent = "";
+    } catch (error) {
+        zipError.textContent = error;
+    }
     });
   
   });
   
+    const save = () => {
+        try{
+            let contact =  createContact();
+        }catch(error){
+            return;
+        }
+    }
+
+    const createContact = () => {
+        let contact = new Contact();
+        try {
+            contact.name = getInputValueById("#name");
+          } catch (error) {
+            setTextValue(".name-error", error);
+            throw error;
+          }
+
+        try {
+            contact.phoneNumber = getInputValueById("#phoneNumber");
+            } catch (error) {
+            setTextValue(".tel-error", error);
+            throw error;
+        }
+    
+        try {
+            contact.address = getInputValueById("#address");
+            } catch (error) {
+            setTextValue(".address-error", error);
+            throw error;
+        }
+
+        try {
+            contact.zip = getInputValueById("#zip");
+            } catch (error) {
+            setTextValue(".zip-error", error);
+            throw error;
+        }
+        
+        let city = getInputValueById("#city");
+            if (city != "Select City") {
+            contact.city = city;
+            } else {
+            throw "Please select city";
+        }
+    
+        let state = getInputValueById("#state");
+            if (state != "Select State") {
+            contact.state = state;
+            } else {
+            throw "Please select state";
+        }
+        alert(contact.toString);
+        return contact;
+  }
+
   const setTextValue = (id, value) => {
     const element = document.querySelector(id);
     element.textContent = value;
   };
+  
+  function getInputValueById(property) {
+    let value = document.querySelector(property).value;
+    return value;
+  }
+  
+  
